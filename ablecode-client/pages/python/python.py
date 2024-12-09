3243bf5f09adb72a668d99da822b243b6e827a1e
AHMAD IBRAHIM <160642197+muallim-khan@users.noreply.github.com>
2024-12-09 11:22:04 +0000

    add pages resources

---
+++ ablecode-client/pages/python/python.py
@@ -0,0 +1,7 @@
+email = input('enter your email:  ')
+password = input('ENTER your password ')
+
+if(email=='wawa.com' and password=='1234')
+     print('login sucesseful')  
+else:
+     print('invalid password or email')      
